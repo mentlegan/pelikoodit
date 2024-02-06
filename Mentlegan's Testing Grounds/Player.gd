b617faed3179c3b36da1d148a6c92b1a5bcffdab
mentlegan <114117731+mentlegan@users.noreply.github.com>
2024-02-06 15:34:20 +0200

    lisättiin testipeliprojekti

---
+++ Mentlegan's Testing Grounds/Player.gd
@@ -0,0 +1,32 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+const MAX_JUMPS = 2
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var current_jumps = 0
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	else:
+		current_jumps = 0
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor() or current_jumps:
+		current_jumps += 1
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
